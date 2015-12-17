e178adb1992cfff8656461ccb2b0d9097def4cb5
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2015-12-17 15:11:33 +0100

    Add support for invoking lambda function from Slack

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ To do a local test use:
 	npm run event
 
 The input data is in `event.json` file.
+
+Slack slash command
+-------------------
+
+This lambda function can also be used as a Slack [Slash command](https://api.slack.com/slash-commands) but because
+Slack sends URL-encoded data in HTTP requests an [additional configuration](https://gist.githubusercontent.com/ryanray/668022ad2432e38493df/raw/a3b8c765791ac6cfc15811a5dcb2d97056adc107/aws-api-gateway-form-to-json.ftl)
+in [API Gateway](https://aws.amazon.com/api-gateway/) is needed.
+
+See [Serverless Slack Integrations with node.js, AWS Lambda, and AWS API Gateway](http://www.ryanray.me/serverless-slack-integrations) for details.
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 require('aws-lambda-web')(exports, function handler(event) {
-    return fetch('https://www.google.com/search?q=' + encodeURIComponent(event.search)).then(function(response) {
+    return fetch('https://www.google.com/search?q=' + encodeURIComponent(event.text)).then(function(response) {
         return response.text();
     }).then(function(text) {
         var rx = /<h3 class="r"><a href="(?:\/url\?q=)?([^&>"]+)[^>]*>(.*?)<\/a><\/h3>/;
         var match = text.match(rx);
         if (match) {
+            // returns a valid response
+            // see: https://api.slack.com/slash-commands
             return {
-                link: match[1],
-                // remove tags, replace numeric entities with Unicode
-                text: match[2].replace(/<(?:.|\n)*?>/gm, '').replace(/&#([0-9]+);/gm, function(_, digits) {
-                    return String.fromCharCode(digits);
-                })
+                response_type: "in_channel",
+                // see: https://api.slack.com/docs/attachments
+                attachments: [{
+                    title_link: match[1],
+                    // remove tags, replace numeric entities with Unicode
+                    title: match[2].replace(/<(?:.|\n)*?>/gm, '').replace(/&#([0-9]+);/gm, function(_, digits) {
+                        return String.fromCharCode(digits);
+                    })
+                }]
             };
         }
     });
